362aad61e0dbd3a78c1ae0bd1b70c19a8593fe6e
Ronal portillo <ronal203010@gmail.com>
2022-07-09 17:40:36 -0700

    challenge 5 optains credsit card nuimbers and gives all but the last 4 digits using astrixs

---
+++ hiddenCreidCard.py
@@ -0,0 +1,26 @@
+# Write a function in Python that accepts a credit card number.
+#  It should return a string where all the characters are hidden 
+#  with an asterisk except the last four. For example,
+#   if the function gets sent "4444444444444444", 
+#   then it should return "************4444".
+
+from distutils.command.sdist import sdist
+
+
+class _User:
+    def __init__(self,ccn):
+        self.cn = ccn
+
+    def display(self):
+        print("**** **** **** "+str(self.cn[-4:]))
+
+if __name__ == "__main__":
+    
+    # get user credit card 
+    cn = input("Enter Credit Card Number: ")
+
+    # create class instance 
+    p1  = _User(cn)
+
+    # print output 
+    p1.display()
