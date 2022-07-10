944a42fff402040a427f8417d004d540f7144c76
Ronal portillo <ronal203010@gmail.com>
2022-07-09 17:45:26 -0700

    further changes

--- hiddenCreidCard.py
+++ hiddenCreidCard.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
     cn = input("Enter Credit Card Number: ")
 
     if len(cn) > 16:
-        print("Invalid Input, Credit card numbers are 16 digits long!")
+        print("Invalid Input, Credit card numbers are atleast 16 digits long!")
     else:
 
     # create class instance 
